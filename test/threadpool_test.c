ff76e2d1866d4b5fa9f4013a2d24b17a8eb79e4e
lamosekm <54715124+lamosekm@users.noreply.github.com>
2024-04-21 18:36:30 -0400

    finsihed assgignment

--- test/threadpool_test.c
+++ test/threadpool_test.c
@@ -131,7 +131,7 @@ static int test_thread_internal(void)
     /* fail when not allowed to use threads */
 
     if (!TEST_uint64_t_eq(OSSL_get_max_threads(NULL), 0))
-    /* Check if OSSL_LIB_CTX_new() succeeded */
+    /* Check if OSSL_LIB_CTX_new() succeeded.  Pushed on GitHub */
         OSSL_LIB_CTX_free(cust_ctx);
         return 0;
     t[0] = ossl_crypto_thread_start(NULL, test_thread_native_fn, &local[0]);
